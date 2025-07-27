795c6eb82169c8be95e6144b7b74774e86a20455
Taha Bokhari <tbokhar@ncsu.edu>
2025-07-26 23:15:33 -0400

    Initial character sheet HTML implementation

--- module/sheets/actor-sheet.mjs
+++ module/sheets/actor-sheet.mjs
@@ -18,7 +18,7 @@ export class HisMajestyTheWormActorSheet extends ActorSheet {
         {
           navSelector: '.sheet-tabs',
           contentSelector: '.sheet-body',
-          initial: 'features',
+          initial: 'biography',
         },
       ],
     });
