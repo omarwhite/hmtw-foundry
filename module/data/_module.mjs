60f2ad1ad8f07d61e7e5e9d525a2e30d66719d26
Taha Bokhari <tbokhar@ncsu.edu>
2025-08-01 22:10:25 -0400

    Try fixing localize issue

--- module/data/_module.mjs
+++ module/data/_module.mjs
@@ -6,7 +6,7 @@ export {default as HisMajestyTheWormNPC} from "./actor-npc.mjs";
 // Export Items
 export {default as HisMajestyTheWormItemBase} from "./base-item.mjs";
 export {default as HisMajestyTheWormItem} from "./item-item.mjs";
-export { default as HisMajestyTheWormBond } from "./item-bond.mjs";
+export {default as HisMajestyTheWormBond} from "./item-bond.mjs";
 export {default as HisMajestyTheWormTalent} from "./item-talent.mjs";
 export {default as HisMajestyTheWormSpell} from "./item-spell.mjs";
-export { default as HisMajestyTheWormCompanion } from "./item-companion.mjs";
+export {default as HisMajestyTheWormCompanion} from "./item-companion.mjs";
