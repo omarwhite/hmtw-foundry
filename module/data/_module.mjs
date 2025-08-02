0b68d9d858b04d9253eb0291bcf2ac966c55934f
Taha Bokhari <tbokhar@ncsu.edu>
2025-08-01 22:29:59 -0400

    Add data models to config

--- module/data/_module.mjs
+++ module/data/_module.mjs
@@ -1,12 +1,12 @@
 // Export Actors
-export {default as HisMajestyTheWormActorBase} from "./base-actor.mjs";
-export {default as HisMajestyTheWormCharacter} from "./actor-character.mjs";
-export {default as HisMajestyTheWormNPC} from "./actor-npc.mjs";
+export { default as HisMajestyTheWormActorBase } from "./base-actor.mjs";
+export { default as HisMajestyTheWormCharacter } from "./actor-character.mjs";
+export { default as HisMajestyTheWormNPC } from "./actor-npc.mjs";
 
 // Export Items
-export {default as HisMajestyTheWormItemBase} from "./base-item.mjs";
-export {default as HisMajestyTheWormItem} from "./item-item.mjs";
+export { default as HisMajestyTheWormItemBase } from "./base-item.mjs";
+export { default as HisMajestyTheWormItem } from "./item-item.mjs";
 export { default as HisMajestyTheWormBond } from "./item-bond.mjs";
-export {default as HisMajestyTheWormTalent} from "./item-talent.mjs";
-export {default as HisMajestyTheWormSpell} from "./item-spell.mjs";
+export { default as HisMajestyTheWormTalent } from "./item-talent.mjs";
+export { default as HisMajestyTheWormSpell } from "./item-spell.mjs";
 export { default as HisMajestyTheWormCompanion } from "./item-companion.mjs";
